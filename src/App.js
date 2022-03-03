eb95d6c73ad7d0bee9077bb6573ae193d1b820b4
alexx1701 <92756383+alexx1701@users.noreply.github.com>
2022-03-02 23:23:42 -0600

    created carousel test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState} from "react";
+import images from "./images";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div className="carousel">
+      <motion.div drag="x" className="inner-carousel">
+        {images.map((image) => {
+          return (
+            <motion.div className="item">
+              <img src={image} alt="" />
+            </motion.div>
+          )
+        })}
+      </motion.div>
+      </motion.div>
     </div>
   );
 }
---
+++ src/images.js
@@ -0,0 +1,7 @@
+import image1 from './images/image1.jpg';
+import image2 from './images/image1.jpg';
+import image3 from './images/image1.jpg';
+import image4 from './images/image1.jpg';
+
+
+export default [image1, image2, image3, image4]
